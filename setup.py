4101f1100c6b2d39874f519d9c6f42cfead46f43
Armin Ronacher <armin.ronacher@active-4.com>
2016-10-18 23:50:11 +0200

    0.4.2

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ class BinaryDistribution(Distribution):
 
 setup(
     name='libsourcemap',
-    version='0.4.1',
+    version='0.4.2',
     url='http://github.com/getsentry/libsourcemap',
     description='Helps working with sourcemaps.',
     license='BSD',
