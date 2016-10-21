c5786b506c4b557ca7a803628142273569ce95eb
Armin Ronacher <armin.ronacher@active-4.com>
2016-10-21 23:06:17 +0200

    0.5.0

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ if bdist_wheel is not None:
 
 setup(
     name='libsourcemap',
-    version='0.4.5',
+    version='0.5.0',
     url='http://github.com/getsentry/libsourcemap',
     description='Helps working with sourcemaps.',
     license='BSD',
