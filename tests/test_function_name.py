c7731f448f599c65e4c9009102301fdfe8b1994f
Armin Ronacher <armin.ronacher@active-4.com>
2017-08-29 16:42:19 +0500

    Add a unicode traceback test

--- tests/test_function_name.py
+++ tests/test_function_name.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from libsourcemap import View
 
 from testutils import get_fixtures
@@ -11,6 +12,23 @@ def test_stacktrace():
         (0, 63, u'e', 'onFailure'),
         (0, 135, 'r', 'invoke'),
         (0, 182, 'i', 'test'),
+        (0, 244, 'nonexisting', None),
+    ]
+
+    for line, col, minified, match in stacktrace:
+        rv = view.get_original_function_name(line, col, minified, min_source)
+        assert rv == match
+
+
+def test_unicode_stacktrace():
+    _, min_source, min_map = get_fixtures('traceback-unicode',
+                                          with_minified=True)
+    view = View.from_json(min_map)
+
+    stacktrace = [
+        (0, 63, u'e', 'onFailure'),
+        (0, 135, 'r', 'invoke'),
+        (0, 191, 'i', u'ÿ'),
         (0, 244, 'nonexisting', None),
     ]
 
